02cf2e6dd07da96c438b7c0b94def0fb9d720929
J. Cohen <hostelmarketing@gmail.com>
2019-12-28 16:28:30 -0800

    Basic functionality works now

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,9 +1,6 @@
 // See https://meta.discourse.org/t/compose-a-new-pre-filled-topic-via-url/28074
 // http://discourse.example.com/new-topic?title=topic%20title&body=topic%20body&category=category/subcategory&tags=email,planned
 
-const DISCOURSE_BASE_URL = "https://forum.codeselfstudy.com";
-const NEW_TOPIC_URL = `${DISCOURSE_BASE_URL}/new-topic`;
-
 // An object that contains references to the elements in the browser action menu
 const links = {
     shareLink: document.getElementById("shareLink"),
@@ -19,56 +16,21 @@ links.shareLink.addEventListener("click", e => {
         })
         .then(sendMessageToTabs)
         .catch(onError);
-    // const sending = browser.tabs.sendMessage(
-    //     1, // tabId, // integer
-    //     { msg: "saluton mondo" }, //message, // any
-    //     {} //options // optional object
-    // );
 });
-
-/**
- * Generate a URL that creates a post draft in Discourse with pre-filled information.
- */
-function sharingUrl(
-    title,
-    body,
-    category = "general-discussion",
-    tagsArr = []
-) {
-    const tags = tagsArr.join(",");
-    const params = new URLSearchParams({
-        title,
-        body,
-        category,
-        tags,
-    }).toString();
-
-    // This URL will create a post draft
-    return `${NEW_TOPIC_URL}/?${params}`;
-}
-
-// copy selected text to use as the body
-
-// extract page title
-function extractPageTitle() {
-    documen;
-}
-
-function onError(error) {
-    console.error(`Error: ${error}`);
-}
 
 function sendMessageToTabs(tabs) {
     for (let tab of tabs) {
         browser.tabs
             .sendMessage(tab.id, { greeting: "Hi from background script" })
             .then(response => {
-                console.log("Message from the content script:");
-                console.log(response.response);
+                console.log("X Message from the content script:");
+                console.log("response XX:", response.response);
+
+                const creating = browser.tabs.create({
+                    url: response.response,
+                });
+                window.close();
             })
-            .catch(onError);
+            .catch(err => console.error(err));
     }
 }
-
-// browser.browserAction.onClicked.addListener(() => {
-// });
--- content_script.js
+++ content_script.js
@@ -1,14 +1,35 @@
 // Put all the javascript code here, that you want to execute after page load.
+const DISCOURSE_BASE_URL = "https://forum.codeselfstudy.com";
+const NEW_TOPIC_URL = `${DISCOURSE_BASE_URL}/new-topic`;
 
 browser.runtime.onMessage.addListener(request => {
     const title = document.title.split("|")[0].trim();
     const body = getSelectionText();
     console.log("title:", title);
     console.log("body:", body);
-    console.log("Message from the background script:");
-    console.log(request.greeting);
-    return Promise.resolve({ response: "Hi from content script" });
+    return Promise.resolve({ response: generateSharingUrl(title, body) });
 });
+
+/**
+ * Generate a URL that creates a post draft in Discourse with pre-filled information.
+ */
+function generateSharingUrl(
+    title,
+    body,
+    category = "general-discussion",
+    tagsArr = []
+) {
+    const tags = tagsArr.join(",");
+    const params = new URLSearchParams({
+        title,
+        body,
+        category,
+        tags,
+    }).toString();
+
+    // This URL will create a post draft
+    return `${NEW_TOPIC_URL}/?${params}`;
+}
 
 /**
  * Capture the selected text or return an empty string.
