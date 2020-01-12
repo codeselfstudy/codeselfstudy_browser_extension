bfd80985ef9c105a842656b86c352610f921dbca
J. Cohen <hostelmarketing@gmail.com>
2020-01-12 10:39:21 -0800

    Transfer popup links into React

--- src/ts/containers/Dashboard.tsx
+++ src/ts/containers/Dashboard.tsx
@@ -4,15 +4,52 @@ import styled from "styled-components";
 export default function Dashboard() {
     return (
         <DashboardContainer>
-            <Display>Dashboard</Display>
+            <h1>
+                <a href="https://codeselfstudy.com/" target="_blank">
+                    Code Self Study
+                </a>
+            </h1>
+            <ul>
+                <li>
+                    <a
+                        title="Share this page in the forum"
+                        href="#"
+                        id="shareLink"
+                    >
+                        Share Current Page
+                    </a>
+                </li>
+                <li>
+                    <a href="https://forum.codeselfstudy.com/" target="_blank">
+                        Coding Forum
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.meetup.com/codeselfstudy"
+                        target="_blank"
+                    >
+                        SF Bay Meetups
+                    </a>
+                </li>
+                <li>
+                    <a href="https://codeselfstudy.slack.com/" target="_blank">
+                        Programming Chatroom
+                    </a>
+                </li>
+                <li>
+                    <a href="#" id="codesLink">
+                        WiFi &amp; Bathroom Codes
+                    </a>
+                </li>
+            </ul>
+            <div id="codesOutput"></div>
         </DashboardContainer>
     );
 }
 
-const DashboardContainer = styled("div")`
-    color: teal;
-`;
+const DashboardContainer = styled("div")``;
 
-const Display = styled("div")`
-    font-size: 48px;
-`;
+// const Display = styled("div")`
+//     font-size: 48px;
+// `;
