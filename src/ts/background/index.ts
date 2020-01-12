d7ea6e6e241b00eff1c28f37d76023f4e4a33bc5
J. Cohen <hostelmarketing@gmail.com>
2020-01-12 07:54:09 -0800

    Reformat with prettier

--- src/ts/background/index.ts
+++ src/ts/background/index.ts
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
-import { wrapStore } from 'webext-redux';
-import { configureApp } from './AppConfig';
-import reducers, { loadState } from './store';
+import { createStore } from "redux";
+import { wrapStore } from "webext-redux";
+import { configureApp } from "./AppConfig";
+import reducers, { loadState } from "./store";
 
 const preloadedState = loadState();
 const store = createStore(reducers, preloadedState);
--- src/ts/contentScripts/counter/index.tsx
+++ src/ts/contentScripts/counter/index.tsx
@@ -1,18 +1,19 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Store } from 'webext-redux';
-import CounterApp from './containers/CounterApp';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Store } from "webext-redux";
+import CounterApp from "./containers/CounterApp";
 
-import { createDomAnchor } from '../../scripts/dom';
+import { createDomAnchor } from "../../scripts/dom";
 
-createDomAnchor('counter-root');
+createDomAnchor("counter-root");
 const store = new Store();
 
 store.ready().then(() => {
-	ReactDOM.render(
-		<Provider store={store}>
-			<CounterApp />
-		</Provider>
-		, document.getElementById('counter-root'));
+    ReactDOM.render(
+        <Provider store={store}>
+            <CounterApp />
+        </Provider>,
+        document.getElementById("counter-root")
+    );
 });
